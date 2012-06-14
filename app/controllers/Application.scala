1c42f10fe51d9a0ed904195ecb09e2b707a00db8
giangvo2011 <giangvo2011@gmail.com>
2012-06-14 11:18:34 +0700

    add spring

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,11 +1,16 @@
 package controllers
 
-import play.api._
-import play.api.mvc._
+import play.api.modules.spring.Spring
+import play.api.mvc.Action
+import play.api.mvc.Controller
+import service.MyService
 
 object Application extends Controller {
   
   def index = Action {
+    val myService = Spring.getBeanOfType(classOf[MyService])
+    myService.go
+    
     Ok(views.html.index("Your new application is ready."))
   }
   
---
+++ app/service/impl/MyServiceImpl.scala
@@ -0,0 +1,11 @@
+package service.impl
+
+import service.MyService
+
+class MyServiceImpl extends MyService {
+
+  def go {
+    println("==== GO GO GO ====")
+  }
+
+}
