bd854d1e0edc29f77e69f4faa27dac7e5a2f296f
giangvo2011 <giangvo2011@gmail.com>
2012-06-14 11:38:24 +0700

    add AbstractController

---
+++ app/controllers/AbstractController.scala
@@ -0,0 +1,10 @@
+package controllers
+
+import play.api.modules.spring.Spring
+import play.api.mvc.Action
+import play.api.mvc.Controller
+import service.MyService
+
+class AbstractController extends Controller{
+  val myService = Spring.getBeanOfType(classOf[MyService])
+}
--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,16 +1,12 @@
 package controllers
 
-import play.api.modules.spring.Spring
 import play.api.mvc.Action
 import play.api.mvc.Controller
-import service.MyService
 
-object Application extends Controller {
+object Application extends AbstractController{
   
   def index = Action {
-    val myService = Spring.getBeanOfType(classOf[MyService])
     myService.go
-    
     Ok(views.html.index("Your new application is ready."))
   }
   
