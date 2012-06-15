f521a34c572aab8f0db7aa2db4918a1cd9bdbaea
tuananhwmn2902 <tuananhwmn2902@gmail.com>
2012-06-15 15:26:43 +0700

    ggg

--- project/Build.scala
+++ project/Build.scala
@@ -11,12 +11,11 @@ object ApplicationBuild extends Build {
       "org.springframework"    %    "spring-context"    %    "3.0.7.RELEASE",
       "org.springframework"    %    "spring-core"       %    "3.0.7.RELEASE",
       "org.springframework"    %    "spring-beans"      %    "3.0.7.RELEASE",
-      "net.databinder" % "dispatch-http_2.9.1" % "0.8.8",
-      "com.google.code" % "morphia" % "0.91"
+      "net.databinder" 		   %    "dispatch-http_2.9.1" %  "0.8.8"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-       
+      // Add your own project settings here      
     )
 
 }
