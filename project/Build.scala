b1ca892605bd122a849b2a04073c58cfe90e98a2
tuananhwmn2902 <tuananhwmn2902@gmail.com>
2012-06-15 11:29:12 +0700

    commit code tuan anh

--- project/Build.scala
+++ project/Build.scala
@@ -11,7 +11,8 @@ object ApplicationBuild extends Build {
       "org.springframework"    %    "spring-context"    %    "3.0.7.RELEASE",
       "org.springframework"    %    "spring-core"       %    "3.0.7.RELEASE",
       "org.springframework"    %    "spring-beans"      %    "3.0.7.RELEASE",
-      "net.databinder" 		   %    "dispatch-http_2.9.1" %  "0.8.8"
+      "net.databinder" % "dispatch-http_2.9.1" % "0.8.8",
+      "com.google.code" % "morphia" % "0.91"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
