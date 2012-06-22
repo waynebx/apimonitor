3998ca45bd4620043f9183621fe211ddc5fd2a5d
giangvo2011 <giangvo2011@gmail.com>
2012-06-22 17:40:11 +0700

    ajax support

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,26 +1,23 @@
 package controllers
 
-import play.api.mvc.Action
-import play.api.mvc.Controller
-import util.APIRequestUtils
-import sjson.json._
-import sjson.json.Serializer.SJSON
-import dispatch.json.Js
 import scala.annotation.target.field
 import scala.reflect.BeanInfo
-import views.html.index$
-import models.ResList
+
+import dispatch.json.Js
 import models.APIResource
-import models.APISpec
-import models.APISpec
 import models.Res
-import scala.collection.mutable.ListBuffer
+import models.ResList
+import play.api.mvc.Action
+import play.api.mvc.Controller
+import sjson.json.Serializer.SJSON
+import sjson.json._
+import util.APIRequestUtils
 
 object Application extends AbstractController {
 
   def index = Action {
 
-    val res = APIRequestUtils.getWS("http://api.sgcharo.com/mobion/resources.json", Map())
+   /* val res = APIRequestUtils.getWS("http://api.sgcharo.com/mobion/resources.json", Map())
     val apis: List[Res] = SJSON.in[ResList](Js(res)).apis
 
     var list = List[APIResource]()
@@ -29,9 +26,34 @@ object Application extends AbstractController {
       val res2 = APIRequestUtils.getWS("http://api.sgcharo.com/mobion" + path + "/list_api?api_key=a3633f30bb4a11e18887005056a70023", Map())
       
       list ::= SJSON.in[APIResource](Js(res2))
+    })*/
+
+    Ok(views.html.index())
+  }
+  
+  def getapi(url : String) = Action{
+    var newUrl = url
+    
+    if(!newUrl.startsWith("http://")){
+      newUrl += "http://"
+    }
+    
+    println(newUrl)
+    
+    val res = APIRequestUtils.getWS(newUrl + "/resources.json", Map())
+    val apis: List[Res] = SJSON.in[ResList](Js(res)).apis
+
+    var list = List[APIResource]()
+    apis.foreach(api => {
+      val path = api.path
+      val res2 = APIRequestUtils.getWS(newUrl + path + "/list_api?api_key=a3633f30bb4a11e18887005056a70023", Map())
+      
+      list ::= SJSON.in[APIResource](Js(res2))
     })
 
-    Ok(views.html.index(list))
+    
+    Ok(views.html.resources_list(list))
+    
   }
 
 }
---
+++ public/javascripts/main.js
@@ -0,0 +1,168 @@
+
+function getAPI()
+{
+	var url = $("#input_baseUrl").val().trim();
+	$("#content_message").html("Loading...");
+	$("#resources_list").slideUp();
+	$("#content_message").slideDown();
+
+	$('#resources_list').load('/getapi?url=' + encodeURIComponent(url), null,
+			function() {
+				$("#content_message").slideUp();
+				$("#resources_list").slideDown();
+				if (supportsLocalStorage) {
+					localStorage.setItem("com.mobion.url", url);
+				}
+			}
+	);
+}
+
+function supportsLocalStorage()
+{
+	try {
+		return 'localStorage' in window && window['localStorage'] !== null;
+	} catch (e) {
+		return false;
+	}
+}
+
+$(function() {
+	if (supportsLocalStorage()) {
+		var url = localStorage.getItem("com.mobion.url", url);
+		$("#input_baseUrl").val(url);
+	}
+});
+
+
+  
+// GUi Effect utils
+var Docs() = {
+		
+		
+		showResponse: function(response, elementScope) {
+		    // log(response);
+		    var prettyJson = JSON.stringify(response, null, "\t").replace(/\n/g, "<br>");
+		    // log(prettyJson);
+		    $(".response_body", elementScope + "_content_sandbox_response").html(prettyJson);
+
+		    $(elementScope + "_content_sandbox_response").slideDown();
+		  },
+
+		  showErrorStatus: function(data,elementScope) {
+		    // log("error " + data.status);
+		    this.showStatus(data,elementScope);
+		    $(elementScope + "_content_sandbox_response").slideDown();
+		  },
+
+		  showCompleteStatus: function(data,elementScope) {
+		    // log("complete " + data.status);
+		    this.showStatus(data,elementScope);
+		  },
+
+		  showStatus: function(data,elementScope) {
+		    // log(data);
+		    // log(data.getAllResponseHeaders());
+		  	var jsonData = JSON.parse(data.responseText);
+		    var response_body = "<pre>" + JSON.stringify(jsonData, null, 2).replace(/\n/g, "<br>") + "</pre>";
+		    if(jsonData.status == "success"){
+		  	  $(".response_code", elementScope + "_content_sandbox_response").html("<pre>" + "OK" + "</pre>");
+		    }else{
+		  	  $(".response_code", elementScope + "_content_sandbox_response").html("<pre>" + jsonData.error_code + "</pre>");  
+		    }
+		    $(".response_body", elementScope + "_content_sandbox_response").html(response_body);
+		    $(".response_headers", elementScope + "_content_sandbox_response").html("<pre>" + data.getAllResponseHeaders() + "</pre>");
+		  }
+
+		shebang: function() {
+
+			// If shebang has an operation nickname in it..
+			// e.g. /docs/#!/words/get_search
+			var fragments = $.param.fragment().split('/');
+			fragments.shift(); // get rid of the bang
+
+			switch (fragments.length) {
+				case 1:
+					// Expand all operations for the resource and scroll to it
+					log('shebang resource:' + fragments[0]);
+					var dom_id = 'resource_' + fragments[0];
+
+					Docs.expandEndpointListForResource(fragments[0]);
+					$("#"+dom_id).slideto({highlight: false});
+					break;
+				case 2:
+					// Refer to the endpoint DOM element, e.g. #words_get_search
+					log('shebang endpoint: ' + fragments.join('_'));
+
+	                // Expand Resource
+	                Docs.expandEndpointListForResource(fragments[0]);
+	                $("#"+dom_id).slideto({highlight: false});
+
+	                // Expand operation
+					var li_dom_id = fragments.join('_');
+					var li_content_dom_id = li_dom_id + "_content";
+
+	                log("li_dom_id " + li_dom_id);
+	                log("li_content_dom_id " + li_content_dom_id);
+
+					Docs.expandOperation($('#'+li_content_dom_id));
+					$('#'+li_dom_id).slideto({highlight: false});
+					break;
+			}
+
+		},
+
+		toggleEndpointListForResource: function(resource) {
+			var elem = $('li#resource_' + resource + ' ul.endpoints');
+			if (elem.is(':visible')) {
+				Docs.collapseEndpointListForResource(resource);
+			} else {
+				Docs.expandEndpointListForResource(resource);
+			}
+		},
+
+		// Expand resource
+		expandEndpointListForResource: function(resource) {
+			$('#resource_' + resource).addClass('active');
+
+			var elem = $('li#resource_' + resource + ' ul.endpoints');
+			elem.slideDown();
+		},
+
+		// Collapse resource and mark as explicitly closed
+		collapseEndpointListForResource: function(resource) {
+			$('#resource_' + resource).removeClass('active');
+
+			var elem = $('li#resource_' + resource + ' ul.endpoints');
+			elem.slideUp();
+		},
+
+		expandOperationsForResource: function(resource) {
+			// Make sure the resource container is open..
+			Docs.expandEndpointListForResource(resource);
+			$('li#resource_' + resource + ' li.operation div.content').each(function() {
+				Docs.expandOperation($(this));
+			});
+		},
+
+		collapseOperationsForResource: function(resource) {
+			// Make sure the resource container is open..
+			Docs.expandEndpointListForResource(resource);
+			$('li#resource_' + resource + ' li.operation div.content').each(function() {
+				Docs.collapseOperation($(this));
+			});
+		},
+
+		expandOperation: function(elem) {
+			elem.slideDown();
+		},
+
+		collapseOperation: function(elem) {
+			elem.slideUp();
+		},
+
+		toggleOperationContent: function(dom_id) {
+			var elem = $('#' + dom_id);
+			(elem.is(':visible')) ? Docs.collapseOperation(elem) : Docs.expandOperation(elem);
+		}
+
+	};
