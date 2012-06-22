e5d1d7265c80be6cf685ff12442744704526efd6
tuananhwmn2902 <tuananhwmn2902@gmail.com>
2012-06-22 11:38:15 +0700

    apply mongo

--- project/Build.scala
+++ project/Build.scala
@@ -11,11 +11,11 @@ object ApplicationBuild extends Build {
       "org.springframework"    %    "spring-context"    %    "3.0.7.RELEASE",
       "org.springframework"    %    "spring-core"       %    "3.0.7.RELEASE",
       "org.springframework"    %    "spring-beans"      %    "3.0.7.RELEASE",
-      "net.databinder" 		   %    "dispatch-http_2.9.1" %  "0.8.8",
       "com.mongodb.casbah" %% "casbah" % "2.1.5-1",
+      "net.databinder" % "dispatch-http_2.9.1" % "0.8.8",
       "com.novus" %% "salat-core" % "0.0.8-SNAPSHOT",
       "mysql" % "mysql-connector-java" % "5.1.18",
-      "org.squeryl" %% "squeryl" % "0.9.5-2"
+      "net.debasishg" % "sjson_2.9.1" % "0.17"     
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
