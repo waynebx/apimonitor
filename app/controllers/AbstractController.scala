4703fb07c684a39f14711342277daa369cca6fd3
giangvo2011 <giangvo2011@gmail.com>
2012-06-27 09:22:17 +0700

    filter response

--- app/controllers/AbstractController.scala
+++ app/controllers/AbstractController.scala
@@ -4,7 +4,18 @@ import play.api.modules.spring.Spring
 import play.api.mvc.Action
 import play.api.mvc.Controller
 import service.MyService
+import play.api.mvc.SimpleResult
+import play.api.mvc.PlainResult
+import play.api.mvc.Result
 
 class AbstractController extends Controller{
   val myService = Spring.getBeanOfType(classOf[MyService])
+  
+    def filterResponse[T](ret: SimpleResult[T]): Result = {
+    ret.withHeaders("Access-Control-Allow-Origin" -> "*")
+  }
+  
+  def filterResponse(ret: PlainResult): Result = {
+    ret.withHeaders("Access-Control-Allow-Origin" -> "*")
+  }
 }
