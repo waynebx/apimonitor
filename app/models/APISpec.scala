bc150750e1281d30f9d07d45261b3b2f80708ed3
tuananhwmn2902 <tuananhwmn2902@gmail.com>
2012-06-29 17:43:53 +0700

    TestCase

--- app/models/APISpec.scala
+++ app/models/APISpec.scala
@@ -2,10 +2,11 @@ package models
 import scala.reflect.BeanInfo
 import sjson.json.JSONTypeHint
 import scala.annotation.target.field
+import com.novus.salat.annotations._
 
-@BeanInfo case class APISpec(@(JSONTypeHint @field)(value = classOf[APIOperation]) operations : List[APIOperation], path: String, description : String, resPath : String) {
+@BeanInfo case class APISpec(@Key("_id") var id:String,@(JSONTypeHint @field)(value = classOf[APIOperation]) operations : List[APIOperation], path: String, description : String, resPath : String,operationsId:List[String]) {
   
   def this() = {
-    this(List(),"","","");
+    this("",List(),"","","",List());
   }
 }
