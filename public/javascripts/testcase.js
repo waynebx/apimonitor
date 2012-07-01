b5018e994f533eee92d6486f06aaedbd494e6a5b
fishsauce <fishsauce.08@gmail.com>
2012-07-01 10:23:05 +0700

    commit

--- public/javascripts/testcase.js
+++ public/javascripts/testcase.js
@@ -16,17 +16,24 @@ var TestCaseMain = Spine.Controller.sub({
 	events : {
 		"click  .save_list_function" : "addAPI2TestCase",
 		"click .add_ope_item"	 : "addOpeItem",
-
 		"click #add_testcase_bt" : "addTestCase",
+		"click .remove_ope_item" : "removeOpeItem"
 
 	},
 	
-
+	removeOpeItem : function(e){
+		$(e.target).html("Add").css("color","#0F6AB4").removeClass().addClass("add_ope_item");
+		
+		var id = "#api_" + $(e.target).attr("id");
+		$(id).remove();
+	},
+	
 	addOpeItem : function(e){
-		$(e.target).html("Added").css("color","red").removeClass().addClass("item_added");
-		var countApiConfigs = $('#add_api_2_testcase_form  .api_configs').size();
 		var apiId = "apiId";
-		var renderString = "<dd class='api_configs'>";
+		$(e.target).html("Added").css("color","red").removeClass().addClass("remove_ope_item").attr("id",apiId);
+		var countApiConfigs = $('#add_api_2_testcase_form  .api_configs').size();
+		
+		var renderString = "<dd class='api_configs' id='api_" + apiId + "'>";
 		renderString += "<input type='text' value='" + apiId + "'  name='apiConfigs[" + countApiConfigs + "].apiId'  />";
 		$(e.target).parents('.operations').find(".content table tbody tr").each(function(){
 			var paramName = $(this).find('.code').html();
@@ -39,7 +46,6 @@ var TestCaseMain = Spine.Controller.sub({
 	
 
 	getListTestCase : function(){
-		console.log("load list");
 //		var url = $("#input_baseUrl").val().trim();
 		var url = "http://api.sgcharo.com/mobion";
 		this.testcase_list.empty();
@@ -57,7 +63,9 @@ var TestCaseMain = Spine.Controller.sub({
 		postJson("/add_test_case", json, function(res) {
 			$("#testcase_list #resources").prepend(res.responseText);
 		});
-
+		var testcaseId = $('#testcase_list #resources li.resource:first').attr("id").split("_")[1];
+		$("#add_api_2_testcase_form").append("<dd><input type='text' name='id' value='"+ testcaseId+ "'/></dd>");
+		$('.thickbox').trigger('click');
 	},
 	
 	addAPI2TestCase : function() {
@@ -72,10 +80,10 @@ var TestCaseMain = Spine.Controller.sub({
 		var json = JSON.stringify(formData, null, '\t');
 		alert(json);
 //
-		postJson("/add_api_to_testcase", json, function(res) {
-			$("#testcase_list #resources").append(res.responseText);
-		});
-		
+//		postJson("/add_api_to_testcase", json, function(res) {
+//			$("#testcase_list #resources").append(res.responseText);
+//		});
+//		
 		
 		$('#add_api_2_testcase_form').html("");
 		$("#TB_closeWindowButton").trigger('click');
